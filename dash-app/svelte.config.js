7a9f866804f10ab7c148d81c998a54c93636cfcd
Alex <88835216+a4004@users.noreply.github.com>
2024-02-23 20:01:23 +0000

    project setup

--- dash-app/svelte.config.js
+++ dash-app/svelte.config.js
@@ -1,17 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [
-		sveltePreprocess({postcss: true}),
-		vitePreprocess()
-	],
-	
+	preprocess: vitePreprocess(),
+
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -19,4 +14,5 @@ const config = {
 		adapter: adapter()
 	}
 };
+
 export default config;
