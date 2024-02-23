d0e8268085420773f956c608ff0b270feb31b418
Alex <88835216+a4004@users.noreply.github.com>
2024-02-23 20:10:04 +0000

    config

---
+++ dash-app/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import "geist-ui-svelte/styles/geist-ui-svelte.css";
+	import { ModeWatcher } from "mode-watcher";
+</script>
+
+<ModeWatcher />
+<slot/>
---
+++ dash-app/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+		"./node_modules/geist-ui-svelte/**/*.{html,js,svelte,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+				gray: {
+					0: "#ffffff",
+					50: "#f9f9f9",
+					100: "#f2f2f2",
+					200: "#d9d9d9",
+					300: "#bfbfbf",
+					400: "#a6a6a6",
+					500: "#8c8c8c",
+					600: "#737373",
+					700: "#595959",
+					800: "#404040",
+					900: "#262626",
+					925: "#1d1d1d",
+					950: "#141414",
+					999: "#000000",
+				},
+			},
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+}
+
